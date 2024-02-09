c32878beb96c894a6b0bcb71cdcfa6cbac655fd9
MH Wael Anjo <wael.tcc.anjo@gmail.com>
2024-02-09 17:56:12 +0300

    Add project files.

---
+++ Anjo.Android.Facebook.AudienceNetwork/Additions/AdComponentFrameLayout.cs
@@ -0,0 +1,77 @@
+﻿using Android.Views; 
+
+namespace Com.Facebook.Ads.Internal.Api
+{
+    public abstract partial class AdComponentFrameLayout
+    {
+        public void SetLayoutParams(ViewGroup.LayoutParams p0)
+        {
+            SetLayoutParams(p0);
+        }
+    }
+
+    public abstract partial class AdNativeComponentView
+    {
+        public void SetLayoutParams(ViewGroup.LayoutParams p0)
+        {
+            SetLayoutParams(p0);
+        }
+    }
+
+    internal partial class IInterstitialAdApiInvoker
+    {
+        IAd.ILoadConfigBuilder? IFullScreenAd.BuildLoadAdConfig()
+        {
+            return BuildLoadAdConfig();
+        }
+
+        IFullScreenAd.IShowConfigBuilder IFullScreenAd.BuildShowAdConfig()
+        {
+            return BuildShowAdConfig();
+        }
+    }
+
+    public partial class InitSettingsBuilder
+    {
+        AudienceNetworkAds.IInitSettingsBuilder AudienceNetworkAds.IInitSettingsBuilder.WithInitListener(AudienceNetworkAds.IInitListener p0)
+        {
+            return WithInitListener(p0);
+        }
+
+        AudienceNetworkAds.IInitSettingsBuilder AudienceNetworkAds.IInitSettingsBuilder.WithMediationService(string p0)
+        {
+            return WithMediationService(p0);
+        }
+
+        AudienceNetworkAds.IInitSettingsBuilder AudienceNetworkAds.IInitSettingsBuilder.WithPlacementIds(IList<string> p0)
+        {
+            return WithPlacementIds(p0);
+        }
+    }
+
+    internal partial class IRewardedVideoAdApiInvoker
+    {
+        IAd.ILoadConfigBuilder IFullScreenAd.BuildLoadAdConfig()
+        {
+            return BuildLoadAdConfig();
+        }
+
+        IFullScreenAd.IShowConfigBuilder IFullScreenAd.BuildShowAdConfig()
+        {
+            return BuildShowAdConfig();
+        }
+    }
+
+    internal partial class IRewardedInterstitialAdApiInvoker
+    {
+        IAd.ILoadConfigBuilder IFullScreenAd.BuildLoadAdConfig()
+        {
+            return BuildLoadAdConfig();
+        }
+
+        IFullScreenAd.IShowConfigBuilder IFullScreenAd.BuildShowAdConfig()
+        {
+            return BuildShowAdConfig();
+        }
+    }
+}
---
+++ Anjo.Android.Facebook.AudienceNetwork/Additions/InterstitialAd.cs
@@ -0,0 +1,141 @@
+﻿
+
+//https://github.com/xamarin/FacebookComponents/issues/187
+namespace Com.Facebook.Ads
+{
+    public partial class AdView
+    {
+        internal partial class IAdViewLoadConfigBuilderInvoker
+        {
+            
+            IAd.ILoadConfigBuilder? IAd.ILoadConfigBuilder.WithBid(string? p0)
+            {
+                return WithBid(p0);
+            }
+
+            IAd.ILoadAdConfig? IAd.ILoadConfigBuilder.Build()
+            {
+                return Build();
+            } 
+        }
+    }
+
+    public partial class InterstitialAd
+    {
+        IAd.ILoadConfigBuilder? IFullScreenAd.BuildLoadAdConfig()
+        {
+            return BuildLoadAdConfig();
+        }
+
+        IFullScreenAd.IShowConfigBuilder IFullScreenAd.BuildShowAdConfig()
+        {
+            return BuildShowAdConfig();
+        }
+
+        internal partial class IInterstitialAdLoadConfigBuilderInvoker
+        { 
+            IAd.ILoadConfigBuilder? IAd.ILoadConfigBuilder.WithBid(string? p0)
+            {
+                return WithBid(p0);
+            }
+
+            IAd.ILoadAdConfig? IAd.ILoadConfigBuilder.Build()
+            {
+                return Build();
+            } 
+        }
+
+        internal partial class IInterstitialAdShowConfigBuilderInvoker
+        { 
+            IFullScreenAd.IShowAdConfig? IFullScreenAd.IShowConfigBuilder.Build()
+            {
+                return Build();
+            }
+        }
+    }
+
+    public partial class RewardedInterstitialAd
+    {
+        IAd.ILoadConfigBuilder IFullScreenAd.BuildLoadAdConfig()
+        {
+            return BuildLoadAdConfig();
+        }
+
+        IFullScreenAd.IShowConfigBuilder IFullScreenAd.BuildShowAdConfig()
+        {
+            return BuildShowAdConfig();
+        }
+
+        internal partial class IRewardedInterstitialAdLoadConfigBuilderInvoker
+        { 
+            IAd.ILoadConfigBuilder? IAd.ILoadConfigBuilder.WithBid(string? p0)
+            {
+                return WithBid(p0);
+            }
+
+            IAd.ILoadAdConfig? IAd.ILoadConfigBuilder.Build()
+            {
+                return Build();
+            } 
+        }
+
+        internal partial class IRewardedInterstitialAdShowConfigBuilderInvoker
+        {
+            IFullScreenAd.IShowAdConfig IFullScreenAd.IShowConfigBuilder.Build()
+            {
+                return Build();
+            }
+        }
+    }
+
+    public partial class RewardedVideoAd
+    {
+        IAd.ILoadConfigBuilder IFullScreenAd.BuildLoadAdConfig()
+        {
+            return BuildLoadAdConfig();
+        }
+
+        IFullScreenAd.IShowConfigBuilder IFullScreenAd.BuildShowAdConfig()
+        {
+            return BuildShowAdConfig();
+        }
+
+        internal partial class IRewardedVideoAdLoadConfigBuilderInvoker
+        { 
+            IAd.ILoadConfigBuilder? IAd.ILoadConfigBuilder.WithBid(string? p0)
+            {
+                return WithBid(p0);
+            }
+
+            IAd.ILoadAdConfig? IAd.ILoadConfigBuilder.Build()
+            {
+                return Build();
+            } 
+        }
+
+        internal partial class IRewardedVideoAdShowConfigBuilderInvoker
+        { 
+            IFullScreenAd.IShowAdConfig? IFullScreenAd.IShowConfigBuilder.Build()
+            {
+                return Build();
+            }
+        }
+    }
+
+    public abstract partial class NativeAdBase
+    {
+        internal partial class INativeAdLoadConfigBuilderInvoker
+        { 
+            IAd.ILoadConfigBuilder? IAd.ILoadConfigBuilder.WithBid(string? p0)
+            {
+                return WithBid(p0);
+            }
+
+            IAd.ILoadAdConfig? IAd.ILoadConfigBuilder.Build()
+            {
+                return Build();
+            }
+             
+        }
+    }
+}
